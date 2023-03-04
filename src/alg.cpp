c4844bb9b3235c138e332bee0a4bb75541d73948
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-03-04 22:57:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,7 +30,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
     double sin = 0;
-    for (uint16_t i = 1; i <= (2*count-1); i++) {
+    for (uint16_t i = 1; i <= count; i++) {
             sin += pown(-1, i - 1)*calcItem(x, 2 * i - 1);
         }
     return sin;
